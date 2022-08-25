468a7e528dff3a8e60a3e07b61f969f9ff0c24d2
Nicklaus <nicklaus.auen@gmail.com>
2022-08-25 12:07:02 -0400

    extremely basic STT

---
+++ main.py
@@ -0,0 +1,34 @@
+# Main File for Virtual Assistant
+# brew install portaudio
+# pip install 'speechrecognition', 'pyaudio', and 'pyttsx3' before running
+import speech_recognition as sr
+import pyttsx3
+
+def main():
+	recognizer = sr.Recognizer()
+
+	# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+		# print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+	while True:
+		try:
+			with sr.Microphone() as mic:
+				recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+				audio = recognizer.listen(mic)
+
+				# Use Google's STT and Get Text Back
+				text = recognizer.recognize_google(audio)
+				text = text.lower()
+
+				print(f"Recognized: {text}")
+		except sr.UnknownValueError():
+			print("Error")
+			# recognizer = sr.Recognizer()
+			continue
+
+
+
+
+
+if __name__ == '__main__':
+	main()
