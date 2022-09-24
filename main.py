28438c5e1c39cd319b540b85ddcef98a3c5962b3
shaclay346 <shaclay346@gmail.com>
2022-09-23 20:29:59 -0400

    response interupttion and activiation seems to be working

--- main.py
+++ main.py
@@ -10,10 +10,49 @@ import threading
 import pdb
 import timer
 import keyboard
+import multiprocessing
+import time
+from threading import Thread
+
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return wrapper
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('volume', 0.7)
+    # changed the speed of the VA, default was 200 wpm, too fast imo
+    engine.setProperty("rate", 175)
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+
+def stop_speaker():
+    print("here")
+    global term
+    term = True
+    t.join()
+
+
+@threaded
+def manage_process(p):
+    global term
+    while p.is_alive():
+        if term:
+            p.terminate()
+            term = False
+        else:
+            continue
 
 
 def get_keyboard_input():
-    print("Press Space Bar to start the virtual assistant")
+   # kill the speaking thread here
 
     while True:
         if keyboard.is_pressed('space'):
@@ -21,15 +60,27 @@ def get_keyboard_input():
             break
 
 
+# this is what you call when you want to give the VA something to say now
+def say(text):
+    '''Uses tts to speak the given text'''
+    global t
+    global term
+    term = False
+    p = multiprocessing.Process(target=speak, args=(text,))
+    p.start()
+    t = manage_process(p)
+
+
 def main():
+    print("Press Space Bar to start the virtual assistant")
     get_keyboard_input()
 
     # Voice Recognizer
     recognizer = sr.Recognizer()
 
     # TTS Initialization
-    converter = pyttsx3.init()
-    converter.setProperty('volume', 0.7)
+    # converter = pyttsx3.init()
+    # converter.setProperty('volume', 0.7)
 
     # for index, name in enumerate(sr.Microphone.list_microphone_names()):
     # print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
@@ -48,25 +99,31 @@ def main():
                 text = text.lower()
                 text = str(text)
 
+                response = ""
                 if("timer" in text):
                     if("cancel" in text):
                         print("cancelling timer ")
                         widgets.cancel_timer()
                     else:
                         widgets.set_timer(text)
+                if("time" in text):
+                    response = widgets.get_time()
 
                 print(f"Recognized: {text}")
+
+                # print something like press space to give a new command
+                if(response != ""):
+                    say(response)
+                    print("press space to say another command")
+                    get_keyboard_input()
+                else:
+                    print("press space to say another command")
+                    get_keyboard_input()
 
         except sr.UnknownValueError:
             # print("Error")
             # recognizer = sr.Recognizer()
             continue
-
-
-def speak(text, converter):
-    '''Uses tts to speak the given text'''
-    converter.say(text)
-    converter.runAndWait()
 
 
 if __name__ == '__main__':
